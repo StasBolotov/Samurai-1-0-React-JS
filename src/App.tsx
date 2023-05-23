0781b0d619f3cba5cf2c7fb79245094b65884b74
StasBolotov <Stashek.web.404@gmail.com>
2023-05-23 13:25:47 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+
+/*function App() {
+  return (
+    <div className="App">
+        <ul>
+            <li>html</li>
+            <li>css</li>
+            <li>js</li>
+            <li>react</li>
+        </ul>
+    </div>
+  );
+}*/
+
+const App = () => {
+    return (
+        <div className="App">
+            <ul>
+                <li>html</li>
+                <li>css</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    );
+}
+
+export default App;
